ef197f1a10415f3af38cb64dd0d0f7c5c6156849
Santiago Quintero <herreraquintero23@gmail.com>
2023-09-10 13:10:44 -0500

    clase Empresa

--- src/main/java/ctr/Controlador.java
+++ src/main/java/ctr/Controlador.java
@@ -8,8 +8,12 @@ import jakarta.servlet.annotation.*;
 @WebServlet(name = "controlador", value = "/controlador")
 public class Controlador extends HttpServlet{
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+        response.setContentType("text/html; charset=UTF-8");
         System.out.println("Controlador");
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        response.getWriter().println("controlador");
     }
 
     public void destroy() {
--- src/main/java/model/Empresa.java
+++ src/main/java/model/Empresa.java
@@ -1,2 +1,61 @@
-package model;public class Empresa {
+package model;
+
+import java.io.Serializable;
+
+public class Empresa implements Serializable{
+    private static final long serialVersionUID = 1L;
+    private String nit;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String clave;
+    private String servicio;
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getServicio() {
+        return servicio;
+    }
+
+    public void setServicio(String servicio) {
+        this.servicio = servicio;
+    }
 }
